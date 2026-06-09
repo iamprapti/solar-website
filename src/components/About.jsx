893a10f19fe7d527e82e0bee5c8e849c6392f198
Prapti Gandhi <praptigandhi14@gmail.com>
2026-06-09 22:09:34 +0530

    Change Components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function About(){
     return (
         <>
-            <div className="bg-orange-300 text-7xl ">About Us</div>
+            <div className="bg-orange-300 text-7xl ">This is About Us page</div>
         </>
     )
 }
